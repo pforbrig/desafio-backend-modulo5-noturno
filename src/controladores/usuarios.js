8a7c00b9a14a489d0d886c9b5c44770769a8dd48
pforbrig <pedroforbrig@gmail.com>
2021-09-27 20:41:28 -0300

    Criando controladores

---
+++ src/controladores/usuarios.js
@@ -0,0 +1,14 @@
+const knex = require('../conexao');
+
+const cadastrarUsuario = async (req, res) => {
+};
+const obterPerfil = async (req, res) => {
+};
+const atualizarPerfil = async (req, res) => {
+};
+
+module.exports = {
+    cadastrarUsuario,
+    obterPerfil,
+    atualizarPerfil
+}
---
+++ src/rotas.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const usuarios = require('./controladores/usuarios');
+const login = require('./controladores/login');
+const verificaLogin = require('./filtros/verificaLogin');
+
+const rotas = express();
+
+// cadastro de usuario
+rotas.post('/usuarios', usuarios.cadastrarUsuario);
+
+// login
+rotas.post('/login', login.logar);
+
+// filtro para verificar usuario logado
+rotas.use(verificaLogin);
+
+// obter e atualizar perfil do usuario logado
+rotas.get('/perfil', usuarios.obterPerfil);
+rotas.put('/perfil', usuarios.atualizarPerfil);
