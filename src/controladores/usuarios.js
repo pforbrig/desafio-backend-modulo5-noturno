fcfd5373cb36d209e7c6294252ac64c1c085bb1e
pforbrig <pedroforbrig@gmail.com>
2021-09-28 11:02:02 -0300

    Criada função de criar usuario

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,6 +1,36 @@
 const knex = require('../conexao');
+const bcrypt = require('bcrypt');
+const schemaCadastroUsuario = require('../validacoes/schemaCadastroUsuario');
 
 const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    try {
+        await schemaCadastroUsuario.validate(req.body);
+
+        const emailExiste = await knex('usuarios').where({ email }).first();
+
+        if (emailExiste) {
+            return res.status(400).json("O email já existe");
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        const usuario = await knex('usuarios').insert({
+            nome,
+            email,
+            senha: senhaCriptografada,
+        }).returning('*');
+
+        if (!usuario) {
+            return res.status(400).json("O usuário não foi cadastrado.");
+        }
+
+        return res.status(200).json(usuario[0]);
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 };
 const obterPerfil = async (req, res) => {
 };
