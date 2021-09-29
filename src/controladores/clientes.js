01859e0721865597f0b4acdbc889cf85308fd6d0
pforbrig <pedroforbrig@gmail.com>
2021-09-28 21:52:46 -0300

    Criada função de cadastrar cliente

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -1,6 +1,41 @@
 const knex = require('../conexao');
+const schemaCadastroCliente = require('../validacoes/schemaCadastroCliente');
 
 const cadastrarCliente = async (req, res) => {
+    const { nome, email, telefone, cpf, cep, logradouro, complemento, bairro, cidade, estado } = req.body;
+
+    try {
+        await schemaCadastroCliente.validate(req.body);
+
+        const cpfExiste = await knex('clientes').where({ cpf, usuario_id: req.usuario.id }).first();
+
+        if (cpfExiste) {
+            return res.status(400).json("Você já tem um cliente com esse CPF!");
+        }
+
+        const cliente = await knex('clientes').insert({
+            nome,
+            email,
+            telefone,
+            cpf,
+            cep,
+            logradouro,
+            complemento,
+            bairro,
+            cidade,
+            estado,
+            usuario_id: req.usuario.id,
+        }).returning('*');
+
+        if (!cliente) {
+            return res.status(400).json("O cliente não foi cadastrado.");
+        }
+
+        return res.status(200).json(cliente[0]);
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 };
 const obterClientes = async (req, res) => {
 };
