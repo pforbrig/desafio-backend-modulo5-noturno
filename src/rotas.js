0db2f14062a2c60d4836bacd5ea469e12ea9107b
pforbrig <pedroforbrig@gmail.com>
2021-10-05 14:02:21 -0300

    Melhoria no filtro

--- src/rotas.js
+++ src/rotas.js
@@ -3,6 +3,7 @@ const usuarios = require('./controladores/usuarios');
 const login = require('./controladores/login');
 const clientes = require('./controladores/clientes');
 const filtroLogin = require('./filtros/filtroLogin');
+const filtroCampoNulo = require('./filtros/filtroCampoNulo');
 
 const rotas = express();
 
@@ -12,8 +13,9 @@ rotas.post('/usuarios', usuarios.cadastrarUsuario);
 // login
 rotas.post('/login', login.logar);
 
-// filtro de autenticação
+// filtros
 rotas.use(filtroLogin);
+rotas.use(filtroCampoNulo);
 
 // obter e atualizar perfil do usuario logado
 rotas.get('/perfil', usuarios.obterPerfil);
